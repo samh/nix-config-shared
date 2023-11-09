cc699018b37f0f55a3c3a4b665231f8ced58d3ad
Sam Hartsfield <sam.hartsfield@acspower.com>
2023-11-09 10:52:35 -0500

    Add shfmt, yadm

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -54,7 +54,10 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    shfmt
+    yadm
+  ];
 
   programs.home-manager.enable = true;
   #programs.git.enable = true;
